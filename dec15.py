b5ee7058bf61ff4cd0b6231042b9f7d0b5bed12a
Xu Cheng <maotouc@gmail.com>
2022-12-16 00:38:06 -0800

    Dec 15

--- dec15.py
+++ dec15.py
@@ -6,11 +6,7 @@ MAX_Y_PART_TWO = 20 if SAMPLE else 4000000
 with open(f"dec15{'-sample' if SAMPLE else ''}.txt") as file:
     sensors = []
     beacons = []
-    sensor_closest_distance = []
-    min_x = None
-    max_x = None
-    min_y = None
-    max_y = None
+    sensor_range = []
 
     for line in file:
         line = line.rstrip()
@@ -22,61 +18,59 @@ with open(f"dec15{'-sample' if SAMPLE else ''}.txt") as file:
         beacon_y = int(parts[9][2:])
         if [beacon_x, beacon_y] not in beacons:
             beacons.append([beacon_x, beacon_y])
-        sensor_closest_distance.append(abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y))
+        sensor_range.append(abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y))
 
-        if min_x is None or min(sensor_x, beacon_x) < min_x:
-            min_x = min(sensor_x, beacon_x)
-        if max_x is None or max(sensor_x, beacon_x) > max_x:
-            max_x = max(sensor_x, beacon_x)
-        if min_y is None or min(sensor_y, beacon_y) < min_y:
-            min_y = min(sensor_y, beacon_y)
-        if max_y is None or max(sensor_y, beacon_y) > max_y:
-            max_y = max(sensor_y, beacon_y)
+    def find_coverage(x_cover_groups):
+        x_coverage = []
+        for x_cover in x_cover_groups:
+            if len(x_coverage) == 0:
+                x_coverage.append([x_cover[0], x_cover[1]])
+            else:
+                if x_cover[1] <= x_coverage[-1][1]:
+                    pass
+                elif x_cover[0] <= x_coverage[-1][1] + 1:
+                    x_coverage[-1][1] = x_cover[1]
+                else:
+                    x_coverage.append([x_cover[0], x_cover[1]])
+        return x_coverage
 
-    def can_place_beacon(x, y):
+    def part_one():
+        x_cover_groups = []
         for index, sensor in enumerate(sensors):
-            distance = abs(sensor[0] - x) + abs(sensor[1] - y)
-            if distance <= sensor_closest_distance[index]:
-                return False
-        return True
+            if abs(sensor[1] - Y_TO_TEST) >= sensor_range[index]:
+                continue
+            x_span = sensor_range[index] - abs(sensor[1] - Y_TO_TEST)
+            x_cover_groups.append([sensor[0] - int(x_span), sensor[0] + int(x_span)])
 
-    no_of_positions = 0
-    to_extend_min = False
-    to_extend_max = False
-    for x in range(min_x, max_x + 1):
-        if [x, Y_TO_TEST] in beacons:
-            continue
+        x_coverage = find_coverage(sorted(x_cover_groups, key=lambda x: x[0]))
 
-        if not can_place_beacon(x, Y_TO_TEST):
-            no_of_positions += 1
-            if x == min_x:
-                to_extend_min = True
-            if x == max_x:
-                to_extend_max = True
+        beacon_deduct = 0
+        for beacon in beacons:
+            if beacon[1] == Y_TO_TEST:
+                for x_cover in x_coverage:
+                    if beacon[0] >= x_cover[0] and beacon[0] <= x_cover[1]:
+                        beacon_deduct += 1
+                        break
 
-    if to_extend_min:
-        x = min_x - 1
-        while not can_place_beacon(x, Y_TO_TEST):
-            no_of_positions += 1
-            x -= 1
-    if to_extend_max:
-        x = max_x + 1
-        while not can_place_beacon(x, Y_TO_TEST):
-            no_of_positions += 1
-            x += 1
+        x_covered_range = -beacon_deduct
+        for x_cover in x_coverage:
+            x_covered_range += x_cover[1] - x_cover[0] + 1
 
-    print(f'Part One: {no_of_positions}')
+        return x_covered_range
 
-    found_beacon = False
-    for x in range(0, MAX_X_PART_TWO + 1):
-        print(x)
+    def part_two():
+        x_cover_groups_by_y = {}
+        for index, sensor in enumerate(sensors):
+            for y_to_test in range(max(0, sensor[1] - sensor_range[index]), min(sensor[1] + sensor_range[index], MAX_Y_PART_TWO) + 1):
+                if y_to_test not in x_cover_groups_by_y:
+                    x_cover_groups_by_y[y_to_test] = []
+                x_span = sensor_range[index] - abs(sensor[1] - y_to_test)
+                x_cover_groups_by_y[y_to_test].append([sensor[0] - int(x_span), sensor[0] + int(x_span)])
+
         for y in range(0, MAX_Y_PART_TWO + 1):
-            if can_place_beacon(x, y):
-                found_beacon = True
-                # print(f'found position ({x}, {y})')
-                break
-        if found_beacon:
-            break
+            x_coverage = find_coverage(sorted(x_cover_groups_by_y.get(y, []), key=lambda x: x[0]))
+            if len(x_coverage) > 1:
+                return 4000000 * (x_coverage[0][1] + 1) + y
 
-    print(f'found position ({x}, {y})')
-    print(f'Part Two: {x * 4000000 + y}')
+    print(f'Part One: {part_one()}')
+    print(f'Part Two: {part_two()}')
